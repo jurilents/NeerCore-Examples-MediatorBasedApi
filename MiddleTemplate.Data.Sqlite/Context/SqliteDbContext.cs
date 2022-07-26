887c57fa70de7a84ba13d0f269736a9cf80a47a3
jurilents <keeeneeek@gmail.com>
2022-07-26 22:53:54 +0300

    Use new NeerCore seeders

--- MiddleTemplate.Data.Sqlite/Context/SqliteDbContext.cs
+++ MiddleTemplate.Data.Sqlite/Context/SqliteDbContext.cs
@@ -12,6 +12,6 @@ public class SqliteDbContext : DbContext, IDatabaseContext
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfigurations();
-        builder.SeedDefaultData();
+        builder.ApplyDataSeeders();
     }
 }
